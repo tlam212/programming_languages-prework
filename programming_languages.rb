87edc6d32aa35e74f409af6089b943a4745d3b4d
Toni Lamont <tlam212@gmail.com>
2019-05-06 18:58:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, lang|
+    lang.each do |attrib, type|
+      new_hash[attrib] = type
+      new_hash[attrib][:style] = []
+      new_hash[attrib][:style] << style
+    end
+  end
+  new_hash
 end
